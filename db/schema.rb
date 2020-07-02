32e6fcd691be8bbcd2df37fa413037c1c7aa3a2a
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2020-07-02 16:56:29 +0900

    create image table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_07_02_075434) do
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "image", null: false
+    t.bigint "item_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_images_on_item_id"
+  end
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "produce", null: false
+    t.integer "price", null: false
+    t.integer "delivery_fee", null: false
+    t.bigint "brand_id"
+    t.bigint "category_id", null: false
+    t.bigint "condition_id", null: false
+    t.bigint "prefecture_id", null: false
+    t.bigint "date_id", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["brand_id"], name: "index_items_on_brand_id"
+    t.index ["category_id"], name: "index_items_on_category_id"
+    t.index ["condition_id"], name: "index_items_on_condition_id"
+    t.index ["date_id"], name: "index_items_on_date_id"
+    t.index ["prefecture_id"], name: "index_items_on_prefecture_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,4 +51,6 @@ ActiveRecord::Schema.define(version: 0) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "images", "items"
+  add_foreign_key "items", "users"
 end
