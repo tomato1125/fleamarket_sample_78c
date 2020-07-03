5b76c3039e948f1da459a2338caaddf0b3bd9dee
mibokatsu1 <mibo.katsu@gmail.com>
2020-07-03 11:28:01 +0900

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :items, only: [:index, :show, :index]
+  resources :items, only: [:index, :show]
   root 'items#index'
 end
 
