a08c7416afada490bb19be9b69d54c6a5b73c8bf
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2020-07-03 14:56:09 +0900

    rebuild Gemfile.lock and delete devise validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
   has_one :profile
   has_many :send_informations
