d592d0573c7cadacdcc6c4a93691dce87490f583
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2020-07-03 15:42:31 +0900

    add validations for nickname,email,password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX}
+  validates :password, presence: true, confirmation: true
+
+
   has_one :profile
   has_many :send_informations
   has_many :items
