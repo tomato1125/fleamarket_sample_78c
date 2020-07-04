08e96d7120496d6a8463580a70cef0abe43caa06
tomato1125 <naokiyoshida1125@icoud.com>
2020-07-04 10:57:15 +0900

    productlisting_page(mark_up)(2)

--- db/migrate/20200703035158_create_items.rb
+++
@@ -1,8 +0,0 @@
-class CreateItems < ActiveRecord::Migration[6.0]
-  def change
-    create_table :items do |t|
-      t.integer :productType_id, null: false, foreign_key: true
-      t.timestamps
-    end
-  end
-end
