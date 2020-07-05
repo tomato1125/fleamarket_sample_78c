3d5bdf1a1aebf5a6d1c491291cc18b724e1c16f2
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2020-07-05 23:08:26 +0900

    add other items lists and edit,delete btn for item-user

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+    @items = Item.where.not(id: @item.id).where(category_id: @item.category_id)
   end
 
   def index
