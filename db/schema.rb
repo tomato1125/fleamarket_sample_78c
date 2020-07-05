09e874b36174ce7cc03d1263ea9da269c8936823
mizuki20020105 <66400695+mizuki20020105@users.noreply.github.com>
2020-07-05 13:39:13 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_080413) do
+ActiveRecord::Schema.define(version: 2020_07_03_054618) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -92,7 +92,7 @@ ActiveRecord::Schema.define(version: 2020_07_02_080413) do
     t.index ["user_id"], name: "index_send_informations_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
