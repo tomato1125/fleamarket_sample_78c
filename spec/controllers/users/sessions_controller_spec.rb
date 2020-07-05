518790bfae50f069e23b17758ab956a0245bab11
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2020-07-05 10:47:44 +0900

    add devise controller tests

--- spec/controllers/users/sessions_controller_spec.rb
+++ spec/controllers/users/sessions_controller_spec.rb
@@ -2,8 +2,24 @@ require 'rails_helper'
 
 RSpec.describe Users::SessionsController, type: :controller do
 
-  describe "signin" do
-
+  describe "login-check" do
+    login_user
+    it "should have a current_user" do
+      expect(subject.current_user).to_not eq(nil)
+    end
   end
+
+  # ログインエラーメッセージのテストは統合テストで実装する物
+  # describe 'sign-in out check' do
+  #   before do
+  #     @request.env['devise.mapping'] = Devise.mappings[:user]
+  #     @user = FactoryBot.create(:user, password: "00000000", email: "kkk@egmail.com")
+  #   end
+  #   it 'ログイン後処理がうまくいかないこと' do
+  #     post :create, user: { password: "password", email: "kkk@gmail.com" }
+  #     user.valid?
+  #     expect(user.errors[:email]).to include("が違います")
+  #   end
+  # end
 
 end
