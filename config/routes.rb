3350fcdac843cd3591c573c5c8a3542f5dfc8b01
tomato1125 <naokiyoshida1125@icoud.com>
2020-07-06 16:18:46 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'users#index'
+  root 'items#index'
   resources :users, only: [:index, :edit, :update, :destroy]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :profiles, only: [:new]
 
