f56a1a1fdb59076e1812069b92a35fe0b98ec8b8
mibokatsu1 <mibo.katsu@gmail.com>
2020-07-06 11:58:53 +0900

    resolve conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,13 @@
 Rails.application.routes.draw do
-<<<<<<< Updated upstream
   root 'items#index'
   resources :users, only: [:index, :edit, :update]
   devise_for :users
-
-  resources :items, only: [:index, :show, :new]
+  resources :items, only: [:index, :show, :new, :buy]
   resources :send_informations, only: [:new, :edit, :update]
-=======
-  resources :users, only: [:edit, :update]
-  devise_for :users
-  resources :items, only: [:index, :show]
-  resources :send_informations, only: [:new, :edit, :update]
-  root 'items#index'
   resources :items do
     member do
       get 'buy'
     end
   end
->>>>>>> Stashed changes
 end
 
