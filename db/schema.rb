0df7d5c21ff3a3b86cff8c961307b26c0fbfc2aa
mibokatsu1 <mibo.katsu@gmail.com>
2020-07-06 11:52:26 +0900

    resolve conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2020_07_02_080413) do
+=======
+ActiveRecord::Schema.define(version: 2020_07_03_054618) do
+>>>>>>> Stashed changes
 
   create_table "credits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "number", null: false
