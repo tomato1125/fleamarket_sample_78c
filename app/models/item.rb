1b095ad59aa9a11b3e3c574feb6a7ec0a776a920
tomato1125 <naokiyoshida1125@icoud.com>
2020-07-06 18:06:23 +0900

    commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :prefecture
-  belongs_to_active_hash :date
+  # belongs_to_active_hash :date
 
 end
