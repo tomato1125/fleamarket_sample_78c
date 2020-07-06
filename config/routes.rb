793adc63125f0b512b7422f902d15607e46c470d
tomato1125 <naokiyoshida1125@icoud.com>
2020-07-06 14:11:59 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :users, only: [:index, :edit, :update]
   devise_for :users
+  resources :users, only: [:index, :edit, :update, :destroy]
 
   resources :items, only: [:index, :show, :new]
   resources :send_informations, only: [:new, :edit, :update]
