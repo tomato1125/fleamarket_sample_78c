8b04b88741e53294a23ae50f5ce660178b95e4c0
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2020-07-06 12:04:58 +0900

    change redirect path after sign-in,sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'items#index'
-  resources :users, only: [:index, :edit, :update, :show]
+
   devise_for :users
+  resources :users, only: [:index, :edit, :update, :show]
 
   resources :items, only: [:index, :show, :new]
   resources :send_informations, only: [:new, :edit, :update]
