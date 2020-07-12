b7a756951fe6f0216161652caeaaf92c25645827
mibokatsu1 <mibo.katsu@gmail.com>
2020-07-12 23:27:53 +0900

    add js into topPage

--- app/assets/javascripts/top_page.js
+++ app/assets/javascripts/top_page.js
@@ -1,45 +1,103 @@
 $(function() {
-  function buildHTML(parent) {
-    let html = `
-                <div class="Itemcategory">
-                  <ul class="itemcategory__parent">
-                    <li class="parent__list">
-                      <a href="#" class="itemcategory__parent--name">
-                        ${parent.name}
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                `
-    return html;
-  };
-  $("#headerCategory").on('click', function() {
-    
-    let insertHTML = '';
-
-    parents = $('.parents').val();
-    $(function(parents) {
-      parent = parents.find(1);
-      console.log(parents.ids);
-      
-    });
-
-
-
-
-    $("#parent-form").on("change",function(){
-      // 親ボックスのidを取得してそのidをAjax通信でコントローラーへ送る
-      let parentValue = document.getElementById("parent-form").value;
-  　　//　("parent-form")は親ボックスのid属性
+  // 子カテゴリーを追加するための処理です。
+    function buildChildHTML(child){
+      var html =`<a class="child_category" id="${child.id}" 
+                  href="/category/${child.id}">${child.name}</a>`;
+      return html;
+    }
+  
+    $(".parent_category").on("mouseover", function() {
+      var id = this.id//どのリンクにマウスが乗ってるのか取得します
+      $(".now-selected-red").removeClass("now-selected-red")//赤色のcssのためです
+      $('#' + id).addClass("now-selected-red");//赤色のcssのためです
+      $(".child_category").remove();//一旦出ている子カテゴリ消します！
+      $(".grand_child_category").remove();//孫、てめえもだ！
       $.ajax({
-        url: '/items/search',
-        type: "GET",
-        data: {
-          parent_id: parentValue // 親ボックスの値をparent_idという変数にする。
-        },
+        type: 'GET',
+        url: '/category/search',//とりあえずここでは、newアクションに飛ばしてます
+        data: {parent_id: id},//どの親の要素かを送ります　params[:parent_id]で送られる
         dataType: 'json'
+      }).done(function(children) {
+        children.forEach(function (child) {//帰ってきた子カテゴリー（配列）
+          var html = buildChildHTML(child);//HTMLにして
+          $(".children_list").append(html);//リストに追加します
+        })
+      });
+    });
+  
+    // 孫カテゴリを追加する処理です　基本的に子要素と同じです！
+    function buildGrandChildHTML(child){
+      var html =`<a class="grand_child_category" id="${child.id}"
+                 href="/category/${child.id}">${child.name}</a>`;
+      return html;
+    }
+  
+    $(document).on("mouseover", ".child_category", function () {//子カテゴリーのリストは動的に追加されたHTMLのため
+      var id = this.id
+      $(".now-selected-gray").removeClass("now-selected-gray");//灰色のcssのため
+      $('#' + id).addClass("now-selected-gray");//灰色のcssのため
+      $.ajax({
+        type: 'GET',
+        url: '/category/search',
+        data: {parent_id: id},
+        dataType: 'json'
+      }).done(function(children) {
+        children.forEach(function (child) {
+          var html = buildGrandChildHTML(child);
+          $(".grand_children_list").append(html);
+        })
+        $(document).on("mouseover", ".child_category", function () {
+          $(".grand_child_category").remove();
+        });
+      });
+    });  
+  });
+
+
+// $(function() {
+//   function buildHTML(parent) {
+//     let html = `
+//                 <div class="Itemcategory">
+//                   <ul class="itemcategory__parent">
+//                     <li class="parent__list">
+//                       <a href="#" class="itemcategory__parent--name">
+//                         ${parent.name}
+//                       </a>
+//                     </li>
+//                   </ul>
+//                 </div>
+//                 `
+//     return html;
+//   };
+//   $("#headerCategory").on('click', function() {
+    
+//     let insertHTML = '';
+
+//     parents = $('.parents').val();
+//     console.log(parents);
+//     $(function(parents) {
+//       parent = parents.find(1);
+      
+      
+//     });
+
+
+
+
+    // $("#headerCategory").on("click",function(){
+      // 親ボックスのidを取得してそのidをAjax通信でコントローラーへ送る
+      // let parentValue = document.getElementById("parent-form").value;
+      // let parentValue = $("#headerCategory").value;
+  　　//　("parent-form")は親ボックスのid属性
+      // $.ajax({
+      //   url: '/items/search',
+      //   type: "GET",
+      //   data: {
+      //     parent_id: parentValue // 親ボックスの値をparent_idという変数にする。
+      //   },
+      //   dataType: 'json'
   　　　　//json形式を指定
-      })
+      // })
 
 
 
@@ -58,24 +116,24 @@ $(function() {
     //   let html2 = parent.name;
     // });
     // $(".itemcategory__parent--name").append(html2);
-  });
+//   });
 
-  $(function() {
+//   $(function() {
             
-    // 配列を変数に格納
-    let arr = ['高崎','前橋','太田', '伊勢崎'];
+//     // 配列を変数に格納
+//     let arr = ['高崎','前橋','太田', '伊勢崎'];
   
-    // each()に引数を指定してループ処理
-    $.each(arr, function(i, val) {
+//     // each()に引数を指定してループ処理
+//     $.each(arr, function(i, val) {
   
-      // コンソールに1つずつ出力
-      console.log(i+':'+val);
+//       // コンソールに1つずつ出力
+//       console.log(i+':'+val);
   
-      // 値が前橋の次から中断
-      return (val != '前橋');
+//       // 値が前橋の次から中断
+//       return (val != '前橋');
   
-    });
-  });
+//     });
+//   });
 
-});
+// });
 
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
     member do
       get 'buy'
       post 'pay'
+      post :search
     end
   end
 
