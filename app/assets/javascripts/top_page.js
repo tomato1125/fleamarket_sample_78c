52684793848612540c357a2485420a7d8a5fbb40
mibokatsu1 <mibo.katsu@gmail.com>
2020-07-12 22:08:38 +0900

    add search action

--- app/assets/javascripts/top_page.js
+++ app/assets/javascripts/top_page.js
@@ -24,6 +24,31 @@ $(function() {
       
     });
 
+
+
+
+    $("#parent-form").on("change",function(){
+      // 親ボックスのidを取得してそのidをAjax通信でコントローラーへ送る
+      let parentValue = document.getElementById("parent-form").value;
+  　　//　("parent-form")は親ボックスのid属性
+      $.ajax({
+        url: '/items/search',
+        type: "GET",
+        data: {
+          parent_id: parentValue // 親ボックスの値をparent_idという変数にする。
+        },
+        dataType: 'json'
+  　　　　//json形式を指定
+      })
+
+
+
+
+
+
+
+
+
     // $.each(parents, function(i, parent) {
     //   insertHTML += buildHTML(parent);
     // });
