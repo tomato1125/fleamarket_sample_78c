41a2d8655e9d5d1500b04975f3c0b8b19dab2eaa
mizuki20020105 <66400695+mizuki20020105@users.noreply.github.com>
2020-07-16 12:53:29 +0900

    New_user_register

--- spec/models/profiles_spec.rb
+++ spec/models/profiles_spec.rb
@@ -12,37 +12,37 @@ describe Profile do
     it "is invalid without a family_name" do
       profile = build(:profile, family_name: nil)
       profile.valid?
-      expect(profile.errors[:family_name]).to include("を入力してください。")
+      expect(profile.errors[:family_name]).to include("は不正な値です")
     end
 
     it "is invalid without a wrong hiragana" do
       profile = build(:profile, family_name:"ヤマダ")
       profile.valid?
-      expect(profile.errors[:family_name]).to include("を全角ひらがなで入力してください。")
+      expect(profile.errors[:family_name]).to include("は不正な値です")
     end
 
     it "is invalid without a first_name" do
       profile = build(:profile, first_name: nil)
       profile.valid?
-      expect(profile.errors[:first_name]).to include("を入力してください。")
+      expect(profile.errors[:first_name]).to include("を入力してください")
     end
 
     it "is invalid without a wrong hiragana" do
       profile = build(:profile, first_name:"タロウ")
       profile.valid?
-      expect(profile.errors[:first_name]).to include("を全角ひらがなで入力してください。")
+      expect(profile.errors[:first_name]).to include("は不正な値です")
     end
 
     it"is invalid without a family_name_kana" do
       profile = build(:profile, family_name_kana: nil)
       profile.valid?
-      expect(profile.errors[:family_name_kana]).to include("を入力してください。")
+      expect(profile.errors[:family_name_kana]).to include("を入力してください")
     end
 
     it "is invalid without a wrong katakana" do
       profile = build(:profile, family_name_kana:"山田")
       profile.valid?
-      expect(profile.errors[:family_name_kana]).to include("を全角カタカナで入力してください。")
+      expect(profile.errors[:family_name_kana]).to include("全角カタカナで入力してください")
     end
 
     it "is invalid without a first_name_kana" do
@@ -54,13 +54,13 @@ describe Profile do
     it "is invalid without a wrong katakana" do
       profile = build(:profile, first_name_kana:"太郎")
       profile.valid?
-      expect(profile.errors[:first_name_kana]).to include("を全角カタカナで入力してください。")
+      expect(profile.errors[:first_name_kana]).to include("全角カタカナで入力してください")
     end
 
     it "is invalid without a birth_date" do
       profile = build(:profile, birth_date: nil)
       profile.valid?
-      expect(profile.errors[:birth_date]).to include("can't be blank")
+      expect(profile.errors[:birth_date]).to include("を入力してください")
     end
 
   end
