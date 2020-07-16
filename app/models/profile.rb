82c329bbc2fac198c2acb9fc97c078c00a435742
HiroSakTech <65761498+HiroSakTech@users.noreply.github.com>
2020-07-16 18:19:21 +0900

    add error messages on profile view

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -6,10 +6,10 @@ class Profile < ApplicationRecord
   VALID_NAME = /\A[ぁ-んァ-ン一-龥]/
   VALID_NAME_KANA = /\A[ァ-ヶー－]+\z/
 
-  validates :family_name, presence: true, format:{with: VALID_NAME,message:"全角で入力してください"}
-  validates :first_name, presence: true, format:{with: VALID_NAME,message:"全角で入力してください"}
-  validates :family_name_kana, presence: true, format:{with: VALID_NAME_KANA, message:"全角カタカナで入力してください"}
-  validates :first_name_kana, presence: true, format:{with: VALID_NAME_KANA, message:"全角カタカナで入力してください"}
+  validates :family_name, presence: true, format:{with: VALID_NAME,message:"は全角で入力してください"}
+  validates :first_name, presence: true, format:{with: VALID_NAME,message:"は全角で入力してください"}
+  validates :family_name_kana, presence: true, format:{with: VALID_NAME_KANA, message:"は全角カタカナで入力してください"}
+  validates :first_name_kana, presence: true, format:{with: VALID_NAME_KANA, message:"は全角カタカナで入力してください"}
   validates :birth_date, presence: true
 end
 
