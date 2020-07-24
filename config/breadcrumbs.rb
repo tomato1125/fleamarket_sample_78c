2bbcb8b460452ef32e0aade9ad44763d497d2d67
mizuki20020105 <66400695+mizuki20020105@users.noreply.github.com>
2020-07-24 18:34:29 +0900

    new

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -1,15 +1,22 @@
+  # crumb コントローラー名_アクション名 do
+  #   link "パンくず名にしたい名前", path
+  #   (parent :親メソッド) ← 子メソッドを記述する場合に用いる
+  # end
+
 crumb :root do
   link "Home", root_path
 end
 
 crumb :users do
   link "#{current_user.nickname}のマイページ", new_user_registration_path
+  parent :root
 end
 
 crumb :profiles_index do
   link "本人確認情報",user_profiles_path(current_user)
   parent :users
 end
+
 
 crumb :send_informations_index do 
   link "お届け先住所", user_send_informations_path(current_user)
@@ -25,6 +32,7 @@ crumb :send_information_edit do
   link "お届け先住所変更", edit_send_information_path
   parent :send_informations_index
 end
+
 
 crumb :new_credit do 
   link "支払方法確認",  new_credit_path
